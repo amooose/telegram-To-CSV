16e10a4901b6617d7e667306f750695c74314b68
an0nymooose <35438152+an0nymooose@users.noreply.github.com>
2019-07-09 19:43:37 -0400

    Update TextIterator.java

--- src/TextIterator.java
+++ src/TextIterator.java
@@ -1,8 +1,3 @@
-//
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by Fernflower decompiler)
-//
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
